f5ec9aa6b44a5bb9dea734286c6894735e4c68d0
Josh Baird <jjb.pub01@protonmail.com>
2022-12-11 15:36:51 -0500

    Add template

---
+++ src/year2022/template.rs
@@ -0,0 +1,31 @@
+
+pub fn part1(input: &str) -> usize {
+	todo!();
+}
+
+pub fn part2(input: &str) -> usize {
+	todo!();
+}
+
+pub fn main() {
+    let input = std::fs::read_to_string("input/2022/dayn.txt").unwrap();
+    dbg!(part1(&input));
+    // dbg!(part2(&input));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE: &'static str = r"";
+
+    #[test]
+    pub fn part1_example() {
+        assert_eq!(0, part1(EXAMPLE));
+    }
+
+    // #[test]
+    // pub fn part2_example() {
+	// 	todo!();
+    // }
+}
