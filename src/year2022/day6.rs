1c4ad6fb7438ac453768bf6cccce4bd3f4f9533a
Josh Baird <jjb.pub01@protonmail.com>
2022-12-10 23:14:11 -0500

    Day 6 part 2

--- src/year2022/day6.rs
+++ src/year2022/day6.rs
@@ -1,30 +1,34 @@
 use hashbag::HashBag;
 
-pub fn part1(input: &str) -> usize {
+pub fn find_distinct(input: &str, window_size: usize) -> usize {
     let data = input.trim().as_bytes();
-    let mut window = HashBag::with_capacity(4);
-    for i in 0..4 {
+    let mut window = HashBag::with_capacity(window_size);
+    for i in 0..window_size {
         window.insert(data[i]);
     }
-    for i in 4..data.len() {
-        if window.set_len() == 4 {
+    for i in window_size..data.len() {
+        if window.set_len() == window_size {
             return i;
         }
-        window.remove(&data[i - 4]);
+        window.remove(&data[i - window_size]);
         window.insert(data[i]);
     }
 
     panic!("nothing found");
 }
 
+pub fn part1(input: &str) -> usize {
+    find_distinct(input, 4)
+}
+
 pub fn part2(input: &str) -> usize {
-    todo!();
+    find_distinct(input, 14)
 }
 
 pub fn main() {
     let input = std::fs::read_to_string("input/2022/day6.txt").unwrap();
     dbg!(part1(&input));
-    // dbg!(part2(&input));
+    dbg!(part2(&input));
 }
 
 #[cfg(test)]
@@ -46,8 +50,12 @@ mod tests {
         assert_eq!(11, part1(EXAMPLE5));
     }
 
-    // #[test]
-    // pub fn part2_example() {
-    //     todo!();
-    // }
+    #[test]
+    pub fn part2_examples() {
+        assert_eq!(19, part2(EXAMPLE1));
+        assert_eq!(23, part2(EXAMPLE2));
+        assert_eq!(23, part2(EXAMPLE3));
+        assert_eq!(29, part2(EXAMPLE4));
+        assert_eq!(26, part2(EXAMPLE5));
+    }
 }
