34f6dea1664205bf7826c4895148f1e267f689e6
Josh Baird <jjb.pub01@protonmail.com>
2025-12-11 23:04:02 -0500

    Day 5 part 2

--- src/year2025/day5.rs
+++ src/year2025/day5.rs
@@ -1,9 +1,12 @@
-use std::ops::RangeInclusive;
+use std::{
+    cmp::{max, min},
+    ops::RangeInclusive,
+};
 
 pub fn main() {
     let input = std::fs::read_to_string("input/2025/day5.txt").unwrap();
     dbg!(part1(&input));
-    // dbg!(part2(&input));
+    dbg!(part2(&input));
 }
 
 pub fn part1(input: &str) -> usize {
@@ -17,7 +20,37 @@ pub fn part1(input: &str) -> usize {
 
 #[allow(dead_code, unused_variables)]
 pub fn part2(input: &str) -> usize {
-    todo!();
+    let (ranges, _items) = parse(input);
+    let ranges = ranges.collect::<Vec<_>>();
+    let mut unmerged = ranges.clone();
+    unmerged.sort_unstable_by_key(|r| (*r.start(), *r.end()));
+    let mut merged = Vec::<RangeInclusive<usize>>::new();
+
+    let mut prev_len;
+    loop {
+        prev_len = unmerged.len();
+        while let Some(to_merge) = unmerged.pop() {
+            if let Some(existing) = merged
+                .iter_mut()
+                .find(|r| r.contains(to_merge.start()) || r.contains(to_merge.end()))
+            {
+                let start = min(*existing.start(), *to_merge.start());
+                let end = max(*existing.end(), *to_merge.end());
+                *existing = start..=end;
+            } else {
+                merged.push(to_merge);
+            }
+        }
+        if merged.len() == prev_len {
+            break;
+        }
+        std::mem::swap(&mut unmerged, &mut merged);
+    }
+
+    merged
+        .iter()
+        .map(|r| r.end() - r.start() + 1)
+        .sum::<usize>()
 }
 
 fn parse(
@@ -61,8 +94,8 @@ mod tests {
         assert_eq!(3, part1(EXAMPLE));
     }
 
-    // #[test]
-    // pub fn part2_example() {
-    // 	assert_eq!(0, part2(EXAMPLE));
-    // }
+    #[test]
+    pub fn part2_example() {
+        assert_eq!(14, part2(EXAMPLE));
+    }
 }
